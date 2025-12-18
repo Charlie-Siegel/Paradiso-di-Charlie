fe1ccd218f6bdd241d90086497c3cd10ea982e76
Charlie-Siegel <info@brainweek.de>
2025-12-18 01:31:12 +0100

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -69,7 +69,7 @@ const loader = new GLTFLoader();
 let modelRoot = null;
 
 loader.load(
-  "./models/magazzino.glb",   // ggf. anpassen
+  "./models/magazzino2.glb",   // ggf. anpassen
   (gltf) => {
     modelRoot = gltf.scene;
     scene.add(modelRoot);
@@ -142,4 +142,5 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
 
